b84adee65ab21161222fba7e494f8c45738a8030
RANJAN SINGH <112189769+ranjansingh9900@users.noreply.github.com>
2023-04-23 22:43:54 +0530

    Update Practies_Questions.py

--- Practies_Questions.py
+++ Practies_Questions.py
@@ -457,6 +457,19 @@ print("System.info")
 print(sys.version_info)
 
 
+# 26.Write a Python program to display the current date and time.
+# Sample Output :
+# Current date and time :
+# 2014-07-05 14:34:14
+
+import datetime
+
+now =datetime.datetime.now()
+print("Current date and time: ")
+print(now.strftime("%Y-%M-%D  %H : %M : %S"))
+
+
+
 
 
 
